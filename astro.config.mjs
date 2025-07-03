03565e18b171fe003b242bebbbca5d45d5dfbd30
Evgeniy Podivilov <evgeniy.podivilov@gmail.com>
2025-07-03 16:37:08 +0100

    chore: :sparkles: configure site and base URL for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://epodivilov.github.io',
+  base: '/curriculum-vitae',
   vite: {
     plugins: [tailwindcss()]
   }
